842ef6e46c7de17773f354bb1e7f8f3e08452030
Ayasono <yuehu@ualberta.ca>
2022-08-26 16:42:08 +0800

    [TEXT]

--- src/utils/link.js
+++ src/utils/link.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Link = ({ href, urlName }) => {
 	function handleClick (e) {
 		e.preventDefault()
-		window.history.pushState({id: 'yuehu'}, '', href)
+		window.history.pushState({}, '', href)
 
 		window.dispatchEvent(new PopStateEvent('popstate', {state: {name: 'yuehu'}}))
 	}
